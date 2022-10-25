83baf537dc756dc8c4f7fd5d1fe2c0b96f4d91c4
Ricardo Costa <ricardocosta101085@gmail.com>
2022-10-25 22:54:10 +0100

    added swagger documentation

---
+++ src/main/java/com/rmmcosta/footballleague/config/SpringFoxConfig.java
@@ -0,0 +1,33 @@
+package com.rmmcosta.footballleague.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Football League APIs",
+                "Apis to register football teams and results and see the classification.",
+                "1.0",
+                "",
+                new Contact("Ricardo Costa", "www.udacity.com", "ricardocosta101085@gmail.com"),
+                "", "", Collections.emptyList());
+    }
+}
